e633e753f6baf809c0cf14d3940fc12df017d482
Vika-Sv <vsvjstin@gmail.com>
2025-02-28 00:38:31 +0200

    Program.cs

--- Lab 1.1.2/Program.cs
+++ Lab 1.1.2/Program.cs
@@ -1,25 +1,31 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
     {
-        Segment segment1 = new Segment();
-        Console.WriteLine($"Довжина (за замовчуванням): {segment1.GetLength():F2}");
-        Console.WriteLine($"Кут з OY: {segment1.GetAngleOY():F2} градусів\n");
+        Console.WriteLine("Введіть x1, y1, z1 або натисніть Enter для значень за замовчуванням:");
+        string input = Console.ReadLine();
 
-        Console.Write("Введіть x1, y1, z1: ");
-        double x1 = double.Parse(Console.ReadLine());
-        double y1 = double.Parse(Console.ReadLine());
-        double z1 = double.Parse(Console.ReadLine());
+        Segment segment;
 
-        Console.Write("Введіть x2, y2, z2: ");
-        double x2 = double.Parse(Console.ReadLine());
-        double y2 = double.Parse(Console.ReadLine());
-        double z2 = double.Parse(Console.ReadLine());
+        if (input == "")
+        {
+            segment = new Segment(); 
+        }
+        else
+        {
+            string[] parts1 = input.Split();
+            Console.WriteLine("Введіть x2, y2, z2:");
+            string[] parts2 = Console.ReadLine().Split();
 
-        Segment segment2 = new Segment(x1, y1, z1, x2, y2, z2);
-        Console.WriteLine($"Довжина: {segment2.GetLength():F2}");
-        Console.WriteLine($"Кут з OY: {segment2.GetAngleOY():F2} градусів");
+            segment = new Segment(
+                double.Parse(parts1[0]), double.Parse(parts1[1]), double.Parse(parts1[2]),
+                double.Parse(parts2[0]), double.Parse(parts2[1]), double.Parse(parts2[2])
+            );
+        }
+
+        Console.WriteLine($"Довжина: {segment.GetLength():F2}");
+        Console.WriteLine($"Кут з OY: {segment.GetAngleOY():F2} градусів");
     }
 }
