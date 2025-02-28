008142341abd1dcdc1150da157ea66ea8726153b
Vika-Sv <vsvjstin@gmail.com>
2025-02-28 14:03:54 +0200

    Lab1.2.cpp

--- Lab1.2/Lab1.2.cpp
+++ Lab1.2/Lab1.2.cpp
@@ -3,28 +3,31 @@
 #include "Segment.h"
 
 int main() {
-
     SetConsoleOutputCP(1251);
 
     double x1, y1, z1, x2, y2, z2;
 
-    std::cout << "Введіть координати початкової точки (x1, y1, z1): ";
-    std::cin >> x1 >> y1 >> z1;
+    std::cout << "Введіть x1, y1, z1: ";
 
-    std::cout << "Введіть координати кінцевої точки (x2, y2, z2): ";
-    std::cin >> x2 >> y2 >> z2;
+    if (!(std::cin >> x1 >> y1 >> z1)) {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        Segment segment;
+        std::cout << "Довжина: " << segment.getLength() << ", Кут з OY: " << segment.getAngleOY() << " градусів\n";
+        return 0;
+    }
 
-    Segment userSegment(x1, y1, z1, x2, y2, z2);
-    Segment seg1;
-    Segment seg2(seg1);
+    std::cout << "Введіть x2, y2, z2: ";
+    if (!(std::cin >> x2 >> y2 >> z2)) {
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        Segment segment;
+        std::cout << "Довжина: " << segment.getLength() << ", Кут з OY: " << segment.getAngleOY() << " градусів\n";
+        return 0;
+    }
 
-    std::cout << "Довжина відрізка: " << userSegment.getLength() << ", Кут з віссю OY: " << userSegment.getAngleOY() << " градусів\n";
-
-    std::cout << "Сегмент 1:\n";
-    std::cout << "Довжина: " << seg1.getLength() << ", Кут з OY: " << seg1.getAngleOY() << " градусів\n\n";
-
-    std::cout << "Сегмент 2 (копія сегмента 1):\n";
-    std::cout << "Довжина: " << seg2.getLength() << ", Кут з OY: " << seg2.getAngleOY() << " градусів\n";
+    Segment segment(x1, y1, z1, x2, y2, z2);
+    std::cout << "Довжина: " << segment.getLength() << ", Кут з OY: " << segment.getAngleOY() << " градусів\n";
 
     return 0;
 }
